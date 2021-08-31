55c8dc2dbb4a474c05265c128407bc61b048cd95
Mohamed Tarek <mohamed82008@gmail.com>
2021-08-31 19:41:09 +1000

    move code from Nonconvex

--- src/NonconvexJuniper.jl
+++ src/NonconvexJuniper.jl
@@ -1,5 +1,16 @@
 module NonconvexJuniper
 
-# Write your package code here.
+export JuniperIpoptAlg, JuniperIpoptOptions, IpoptAlg, IpoptOptions
+
+import JuMP, Ipopt, MathOptInterface
+const MOI = MathOptInterface
+using Reexport, Parameters, SparseArrays, Zygote, NonconvexIpopt
+@reexport using NonconvexCore
+using NonconvexCore: @params, VecModel, AbstractResult, JuMPProblem
+using NonconvexCore: AbstractOptimizer, get_jump_problem, nvalues
+import NonconvexCore: optimize!
+using Juniper
+
+include("juniper.jl")
 
 end
