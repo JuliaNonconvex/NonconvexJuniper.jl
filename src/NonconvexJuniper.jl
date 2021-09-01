32b3a2a0a0895d5306ee375e86be44e3b9be449f
Mohamed Tarek <mohamed82008@gmail.com>
2021-09-01 15:32:47 +1000

    reexport NonconvexIpopt

--- src/NonconvexJuniper.jl
+++ src/NonconvexJuniper.jl
@@ -4,8 +4,8 @@ export JuniperIpoptAlg, JuniperIpoptOptions, IpoptAlg, IpoptOptions
 
 import JuMP, Ipopt, MathOptInterface
 const MOI = MathOptInterface
-using Reexport, Parameters, SparseArrays, Zygote, NonconvexIpopt
-@reexport using NonconvexCore
+using Reexport, Parameters, SparseArrays, Zygote
+@reexport using NonconvexCore, NonconvexIpopt
 using NonconvexCore: @params, VecModel, AbstractResult, JuMPProblem
 using NonconvexCore: AbstractOptimizer, get_jump_problem, nvalues
 import NonconvexCore: optimize!
